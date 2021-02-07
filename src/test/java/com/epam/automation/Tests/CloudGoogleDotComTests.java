29859758e6e6be050fab9eb1366622b5381d2a58
Vladimir <ryzhenkov_v@list.ru>
2021-02-07 05:05:13 +0500

    Added Driver Singleton Functionality

--- src/test/java/com/epam/automation/Tests/CloudGoogleDotComTests.java
+++ src/test/java/com/epam/automation/Tests/CloudGoogleDotComTests.java
@@ -1,6 +1,7 @@
 package com.epam.automation.Tests;
 
 import com.epam.automation.PageObject.CloudGoogleDotComPageObject;
+import com.epam.automation.driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -8,12 +9,13 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class CloudGoogleDotComTests {
-    private WebDriver driver;
+public class CloudGoogleDotComTests extends CommonConditions{
+
     private CloudGoogleDotComPageObject webPage;
-    @BeforeMethod(alwaysRun = true)
-    public  void resetBrowser() {
-        driver=new ChromeDriver();
+
+
+    @Test(description = "Test for https://cloud.google.com/")
+    public  void checkMonthlyPayment() {
         webPage=new CloudGoogleDotComPageObject(driver)
                 .openPage()
                 .submitSearchRequest()
@@ -24,17 +26,8 @@ public class CloudGoogleDotComTests {
                 .generateTemporaryEmail()
                 .fillEmailEstimateForm()
                 .clickSendEmail();
-    }
-
-    @Test(description = "Test for https://cloud.google.com/")
-    public  void checkMonthlyPayment() {
         Assert.assertEquals(webPage.getTotalEstimatedCostFromEmail(), 1082, 1.0
                 , "Monthly payment value differ from expected!");
     }
 
-    @AfterMethod(alwaysRun = true)
-    public  void quiteBrowser() {
-        driver.quit();
-        driver=null;
-    }
 }
---
+++ src/test/java/com/epam/automation/Tests/CommonConditions.java
@@ -0,0 +1,28 @@
+package com.epam.automation.Tests;
+
+
+import com.epam.automation.driver.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+
+
+public class CommonConditions {
+
+    protected WebDriver driver;
+
+
+    @BeforeMethod()
+    public void setUp()
+    {
+        driver = DriverSingleton.getDriver();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopBrowser()
+    {
+        DriverSingleton.closeDriver();
+    }
+}
+
