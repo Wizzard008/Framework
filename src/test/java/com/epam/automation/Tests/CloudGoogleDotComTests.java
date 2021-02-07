8b2235eeb71789ae353657001e5df2ece41926ab
Vladimir <ryzhenkov_v@list.ru>
2021-02-07 12:32:54 +0500

    Added two more test from Hart Me Planty task

--- src/test/java/com/epam/automation/Tests/CloudGoogleDotComTests.java
+++ src/test/java/com/epam/automation/Tests/CloudGoogleDotComTests.java
@@ -9,13 +9,46 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class CloudGoogleDotComTests extends CommonConditions{
 
     private CloudGoogleDotComPageObject webPage;
+    @Test(description = "Test for https://cloud.google.com/")
+    public  void checkEstimateFields() {
+        webPage=new CloudGoogleDotComPageObject(driver)
+                .openPage()
+                .submitSearchRequest()
+                .openRequestedSearchResult()
+                .fillEstimateFormFields()
+                .submitAddToEstimate();
+        List<String> estimateFieldsValues=webPage.getEstimateFormValues();
+        Boolean checkVMClass=estimateFieldsValues.get(1).equalsIgnoreCase("VM class: regular");
+        Boolean checkInstanceType=estimateFieldsValues.get(2).equalsIgnoreCase("Instance type: n1-standard-8");
+        Boolean checkRegion=estimateFieldsValues.get(3).equalsIgnoreCase("Region: Frankfurt");
+        Boolean checkLocalSSD=estimateFieldsValues.get(4).equalsIgnoreCase("Total available local SSD space 2x375 GiB");
+        Boolean checkCommitmentTerm=estimateFieldsValues.get(5).equalsIgnoreCase("Commitment term: 1 Year");
+
+        Assert.assertTrue(checkVMClass&checkInstanceType&checkRegion&checkLocalSSD&checkCommitmentTerm
+                ,"Estimate form values differ from expected!");
+    }
+
+    @Test(description = "Test for https://cloud.google.com/")
+    public  void checkMonthlyPaymentFromForm() {
+        webPage=new CloudGoogleDotComPageObject(driver)
+                .openPage()
+                .submitSearchRequest()
+                .openRequestedSearchResult()
+                .fillEstimateFormFields()
+                .submitAddToEstimate();
+
+        Assert.assertEquals(webPage.getTotalEstimatedCost(), 1082.77, 0.0
+                , "Monthly payment value differ from expected!");
+    }
 
 
     @Test(description = "Test for https://cloud.google.com/")
-    public  void checkMonthlyPayment() {
+    public  void checkMonthlyPaymentFromEmail() {
         webPage=new CloudGoogleDotComPageObject(driver)
                 .openPage()
                 .submitSearchRequest()
@@ -29,5 +62,7 @@ public class CloudGoogleDotComTests extends CommonConditions{
         Assert.assertEquals(webPage.getTotalEstimatedCostFromEmail(), 1082, 1.0
                 , "Monthly payment value differ from expected!");
     }
+
+
 
 }
