b294404c328e1e2436025ebf62b6d90837af81bd
Vladimir <ryzhenkov_v@list.ru>
2021-02-08 15:16:44 +0500

    Added TestDataReader class to obtain Test Data from Evironment

--- src/test/java/com/epam/automation/service/UserCreator.java
+++ src/test/java/com/epam/automation/service/UserCreator.java
@@ -3,29 +3,29 @@ package com.epam.automation.service;
 import com.epam.automation.model.User;
 
 public class UserCreator {
-    public static final String USER_NAME="test.user.name";
-    public static final String USER_SURNAME="test.user.surname";
-    public static final String EMAIL_ADDRESS="test.email.address";
-    public static final String PROJECT = "test.project";
-    public static final String TITLE = "test.title";
-    public static final String COMPANY = "test.company";
+    public static final String USER_NAME = "testdata.user.name";
+    public static final String USER_SURNAME = "testdata.user.surname";
+    public static final String EMAIL_ADDRESS = "testdata.email.address";
+    public static final String PROJECT = "testdata.project";
+    public static final String TITLE = "testdata.title";
+    public static final String COMPANY = "testdata.company";
 
     public static User generateUserWithDefaultProperties(){
-//        return new User(
-//                TestDataReader.getTestData(USER_NAME),
-//                TestDataReader.getTestData(USER_SURNAME),
-//                TestDataReader.getTestData(EMAIL_ADDRESS),
-//                TestDataReader.getTestData(PROJECT),
-//                TestDataReader.getTestData(TITLE),
-//                TestDataReader.getTestData(COMPANY)
-//        );
         return new User(
-                USER_NAME,
-                USER_SURNAME,
-                EMAIL_ADDRESS,
-                PROJECT,
-                TITLE,
-                COMPANY
+                TestDataReader.getTestData(USER_NAME),
+                TestDataReader.getTestData(USER_SURNAME),
+                TestDataReader.getTestData(EMAIL_ADDRESS),
+                TestDataReader.getTestData(PROJECT),
+                TestDataReader.getTestData(TITLE),
+                TestDataReader.getTestData(COMPANY)
         );
+//        return new User(
+//                USER_NAME,
+//                USER_SURNAME,
+//                EMAIL_ADDRESS,
+//                PROJECT,
+//                TITLE,
+//                COMPANY
+//        );
     }
 }
